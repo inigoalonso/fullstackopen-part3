b9cc882bcf867b77a7164e28d90729d6c4b89cff
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-04 14:18:02 +0200

    3.2

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -49,6 +49,17 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+app.get('/info', (request, response) => {
+    const personsCount = persons.length;
+    const requestTime = new Date();
+
+    response.send(`
+        <p>Phonebook has info for ${personsCount} people</p>
+        <p>${requestTime}</p>
+    `)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
