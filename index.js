06b3263b90701131886bacd775900dab2362f7c0
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-05 13:48:24 +0200

    3.13

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan');
 const app = express()
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 // Enable CORS
 app.use(cors())
@@ -17,46 +20,35 @@ morgan.token('body', (req) => JSON.stringify(req.body)); // Custom token to log 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')); // From https://www.npmjs.com/package/morgan
 //app.use(morgan('tiny'));
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// Get command line arguments
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+// Get all persons from the MongoDB
+let persons = Person.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(person => {
+        console.log(`${person.name} ${person.number}`);
+    });
+});
+
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
+    Person.findById(id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).send({ error: 'Person not found' });
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -88,14 +80,16 @@ app.post('/api/persons', (request, response) => {
     }
 
     const newId = Math.floor(Math.random() * 10000000000);
-    const newPerson = {
+    const newPerson = new Person({
         id: newId.toString(),
         name: body.name,
         number: body.number
-    };
+    });
 
-    persons = persons.concat(newPerson);
-    response.json(newPerson);
+    person.save().then(savedPerson => {
+        console.log(`added ${savedPerson.name} number ${savedPerson.number} to phonebook`);
+        response.json(savedPerson);
+    });
 });
 
 app.get('/info', (request, response) => {
---
+++ models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+
+// Define the URL for the MongoDB database
+const url = process.env.MONGODB_URI
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false)
+console.log('connecting to', url)
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('person', personSchema)
