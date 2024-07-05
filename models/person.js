6e66cb2c3aa61431f57fa3e0d0912b6fb3f3ef2b
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-05 16:23:22 +0200

    formatting

--- models/person.js
+++ models/person.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 // Define the URL for the MongoDB database
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 // Connect to MongoDB
-mongoose.set('strictQuery', false)
-console.log('connecting to', url)
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
+mongoose.set("strictQuery", false);
+console.log("connecting to", url);
+mongoose
+    .connect(url)
+    .then((result) => {
+        console.log("connected to MongoDB");
     })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message);
+    });
 
 const personSchema = new mongoose.Schema({
     name: String,
     number: String,
-})
+});
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
 
-module.exports = mongoose.model('person', personSchema)
+module.exports = mongoose.model("person", personSchema);
